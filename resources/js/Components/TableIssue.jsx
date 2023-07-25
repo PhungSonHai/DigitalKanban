640db0fa7c09d43c1d71453fe6d41507e99bdb9d
PhungSonHai <phungsonhaistorage@gmail.com>
2023-07-25 13:18:17 +0700

    add loading

--- resources/js/Components/TableIssue.jsx
+++ resources/js/Components/TableIssue.jsx
@@ -23,6 +23,10 @@ function TableIssue() {
         not_solved_issue: 0
     });
     const [scoreEvaluate, setScoreEvaluate] = useState(0);
+    const [isAddIssueStitching, setIsAddIssueStitching] = useState(false);
+    const [isAddIssueMachining, setIsAddIssueMachining] = useState(false);
+    const [isConfirmCancelIssue, setIsConfirmCancelIssue] = useState(false);
+    const [isConfirmCompleteIssue, setIsConfirmCompleteIssue] = useState(false);
 
     useEffect(function () {
         setCheck(() =>
@@ -110,6 +114,7 @@ function TableIssue() {
     }
     
     const handleCompleteIssue = () => {
+        setIsConfirmCompleteIssue(true)
         var data = {}
         data.id = idCompleteIssue
 
@@ -141,6 +146,7 @@ function TableIssue() {
     
                     handleCloseModalComplete()
                     handleGetAllIssueOfLine(staffDepartment)
+                    setIsConfirmCompleteIssue(false)
                 }
             })
             .catch(err => {
@@ -170,6 +176,7 @@ function TableIssue() {
     }
 
     const handleCancelIssue = () => {
+        setIsConfirmCancelIssue(true)
         var data = {}
         data.id = idCancelIssue
 
@@ -198,9 +205,10 @@ function TableIssue() {
                             </div>
                         ),
                     });
-
+                    
                     handleCloseModalCancel()
                     handleGetAllIssueOfLine(staffDepartment)
+                    setIsConfirmCancelIssue(false)
                 }
             })
             .catch(err => {
@@ -526,9 +534,11 @@ function TableIssue() {
         var dataIssue = [];
         if(stage === "S") {
             dataIssue = getDataIssueStitching()
+            setIsAddIssueStitching(true)
         } 
         else if(stage === "L") {
             dataIssue = getDataIssue()
+            setIsAddIssueMachining(true)
         }
 
         if(dataIssue.some(item => item === "")) {
@@ -595,9 +605,11 @@ function TableIssue() {
 
                             if(stage === "S") {
                                 handleCloseModalStitching()
+                                setIsAddIssueStitching(false)
                             }
                             else if(stage === "L") {
                                 handleCloseModalMachining()
+                                setIsAddIssueMachining(false)
                             }
                         }
                     })
@@ -624,6 +636,8 @@ function TableIssue() {
                                 </div>
                             ),
                         });
+                        setIsAddIssueStitching(false)
+                        setIsAddIssueMachining(false)
                     })
             } else {
                 const key = enqueueSnackbar("Xảy ra lỗi không thể thêm vấn đề", {
@@ -648,6 +662,8 @@ function TableIssue() {
                         </div>
                     ),
                 });
+                setIsAddIssueStitching(false)
+                setIsAddIssueMachining(false)
             }
         }
     }, [check, checkStitching])
@@ -946,8 +962,35 @@ function TableIssue() {
                             type="button"
                             className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
                             onClick={() => handleAddIssue('S')}
+                            disabled={isAddIssueStitching}
                         >
-                            Xác nhận
+                            {
+                                isAddIssueStitching
+                                ?
+                                    <div className='flex justify-center' role="status">
+                                        <svg
+                                            aria-hidden="true"
+                                            class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentFill"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                :
+                                    "Xác nhận"
+                            }
                         </button>
                         <button
                             type="button"
@@ -1198,8 +1241,35 @@ function TableIssue() {
                             type="button"
                             className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
                             onClick={() => handleAddIssue('L')}
+                            disabled={isAddIssueMachining}
                         >
-                            Xác nhận
+                            {
+                                isAddIssueMachining
+                                ?
+                                    <div className='flex justify-center' role="status">
+                                        <svg
+                                            aria-hidden="true"
+                                            class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentFill"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                :
+                                    "Xác nhận"
+                            }
                         </button>
                         <button
                             type="button"
@@ -1229,8 +1299,35 @@ function TableIssue() {
                             type="button"
                             className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
                             onClick={() => handleCancelIssue()}
+                            disabled={isConfirmCancelIssue}
                         >
-                            Có
+                            {
+                                isConfirmCancelIssue
+                                ?
+                                    <div className='flex justify-center' role="status">
+                                        <svg
+                                            aria-hidden="true"
+                                            class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentFill"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                :
+                                    "Có"
+                            }
                         </button>
 
                         <button
@@ -1261,8 +1358,35 @@ function TableIssue() {
                             type="button"
                             className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
                             onClick={() => handleCompleteIssue()}
+                            disabled={isConfirmCompleteIssue}
                         >
-                            Có
+                            {
+                                isConfirmCompleteIssue
+                                ?
+                                    <div className='flex justify-center' role="status">
+                                        <svg
+                                            aria-hidden="true"
+                                            class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentFill"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">
+                                            Loading...
+                                        </span>
+                                    </div>
+                                :
+                                    "Có"
+                            }
                         </button>
 
                         <button
